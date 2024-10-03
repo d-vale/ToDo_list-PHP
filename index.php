be553a954a1a9d7fce664e87cada95cad45f72a5
Vince <jpinard@bluewin.ch>
2024-10-03 11:52:00 +0200

    Ajout de SELECT dans la variable

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT'; // IMPLEMENT ME - Done
 $items = $db->query($selectQuery);
 ?>
 
