440ef1fe5cf4685f3118ac345434dad5fe1df2e3
Dvale <vale.daniel@outlook.com>
2024-10-03 11:58:06 +0200

    Ajout de UPDATE et DELETE dans les variables

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = ''; // IMPLEMENT ME
+        $updateQuery = 'UPDATE'; // IMPLEMENT ME - done
         if(!$db->query($updateQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -60,7 +60,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = ''; // IMPLEMENT ME
+        $deleteQuery = 'DELETE'; // IMPLEMENT ME - done
         if(!$db->query($deleteQuery)) {
           die(print_r($db->errorInfo(), true));
         }
