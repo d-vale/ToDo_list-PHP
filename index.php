1081ec85f10b2e0a0d30ec80b36dc5978b3a45bf
Vince <jpinard@bluewin.ch>
2024-10-03 12:16:25 +0200

    Correction l'erreur a la ligne 81 changement de la valeur de la variabe

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = 'SELECT'; // IMPLEMENT ME - Done
+$selectQuery = 'SELECT id, title, done FROM todo';
 $items = $db->query($selectQuery);
 ?>
 
